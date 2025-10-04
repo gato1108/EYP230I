6292325dd1cea5e1576fb5bfc51ff7dbd97867c0
Diego Pérez <dtperez1@uc.cl>
2025-10-04 00:01:22 -0300

    Errores

--- T1/T1_EYP230I copy.R
+++ T1/T1_EYP230I copy.R
@@ -50,7 +50,6 @@ train <- train %>% mutate(hr_baja = ifelse(hr %in% c(0, 1, 2, 3, 4, 5), 1, 0))
 
 
 train <- train %>% select(-casual, -registered)
-test <- test %>% select(-casual, -registered)
 
 ############################
 ## Gráficos de frecuencia ##
@@ -97,3 +96,34 @@ r2_results <- sort(r2_results, decreasing = TRUE)
 r2_df <- data.frame(Variable = names(r2_results), R2 = r2_results)
 
 print(r2_df)
+
+################
+## Predicción ##
+################
+
+test <- dummy_cols(test, select_columns = "hr", remove_first_dummy = TRUE, remove_selected_columns = FALSE)
+test <- dummy_cols(test, select_columns = "mnth", remove_first_dummy = TRUE, remove_selected_columns = FALSE)
+test <- dummy_cols(test, select_columns = "season", remove_first_dummy = TRUE, remove_selected_columns = FALSE)
+test <- dummy_cols(test, select_columns = "weathersit", remove_first_dummy = TRUE, remove_selected_columns = FALSE)
+
+test <- test %>% mutate(hr_almu = ifelse(hr %in% c(12, 13, 14, 15, 7, 9 ,20, 11), 1, 0))
+test <- test %>% mutate(summer = ifelse(mnth %in% c('Jan', 'July', 'June', 'May', 'Sept'), 1, 0))
+test <- test %>% mutate(winter = ifelse(mnth %in% c('Jan', 'Feb', 'March'), 1, 0))
+test <- test %>% mutate(hr_peak = ifelse(hr %in% c(17, 18), 1, 0))
+test <- test %>% mutate(hr_baja = ifelse(hr %in% c(0, 1, 2, 3, 4, 5), 1, 0))
+
+test <- test %>% select(-casual, -registered)
+
+predictions <- predict(mod6, newdata = test)
+print(predictions)
+
+error <- predictions-test$bikers
+
+ggplot(predictions-test$bikers, aes(x = cut, y = counts))
+summary(predictions-test$bikers)
+
+hist(error,
+     main = "Distribución de errores de predicción",
+     xlab = "Error",
+     ylab = "Frecuencia",
+     col = "skyblue", border = "white", breaks = 20)
